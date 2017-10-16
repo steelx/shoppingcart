99332a935b8eabfc1c685f6699f122e1162bec0a
Steel <ajinkya@riva.co>
2017-10-16 10:38:07 +0530

    app js cleaned

--- src/app.js
+++ src/app.js
@@ -2,8 +2,6 @@
 import {createStore, applyMiddleware} from 'redux';
 import reducers from './reducers';
 import logger from 'redux-logger';
-import {addToCart, updateCart, deleteFromCart} from "./actions/cartActions";
-import {addProduct, updateProduct} from "./actions/productsActions";
 
 import React from 'react';
 import {render} from 'react-dom';
@@ -13,13 +11,6 @@ import ProductsList from "./components/productsList";
 // store
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
-
-// Dispach actions
-// store.dispatch(addProduct({id:3, title: 'Banana', description: 'some BANANA peal them pls', price: 10}));
-// store.dispatch(updateProduct({id:1, title: 'NEW Apples'}));
-//
-// store.dispatch(addToCart({id:3, units: 1}));
-// store.dispatch(updateCart({id:1, units: 3}));
 
 render(<Provider store={store}>
     <ProductsList/>
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude:/node_modules/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015', 'stage-1']
                 }
